7b1e7d538fdd3041cee06225250e8f9bc4d1ed14
KrlosPK <centille095@gmail.com>
2022-09-11 17:11:40 -0500

    Hola mundo

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sportfit";
+
+    try {
+        $conn = new PDO ("mysql:host=$server;dbname=$database;",$username, $password);
+    } catch (PDOException $e) {
+        die("Conection Failed: ".$e->getMessage());
+    }
+?>

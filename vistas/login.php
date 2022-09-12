2ab2068f34a0e459019da39ff854061a167e3f53
KrlosPK <centille095@gmail.com>
2022-09-12 08:44:09 -0500

    Login and register

--- vistas/login.php
+++ vistas/login.php
@@ -1,78 +1,80 @@
+<!-- Validaciones para el inicio de sesión de usuarios -->
 <?php
+	session_start();
+
+	if (isset($_SESSION['user_id'])) {
+		header('Location: ../index.php');
+	}
 	require '../php/conexion.php';
 	
-	if (!empty($_POST["documento"]) && !empty($_POST["password"])) {
-		$sql = "INSERT INTO usuarios";
+	if (!empty($_POST['correo']) && !empty($_POST['contrasena'])) {
+		$records = $conn->prepare('SELECT id, correo, contrasena FROM usuarios WHERE correo = :correo');
+		$records->bindParam(':correo', $_POST['correo']);
+		$records->execute();
+		$results = $records->fetch(PDO::FETCH_ASSOC);
+		
+		$message = "";
+
+		if (count($results) > 0 && password_verify($_POST['contrasena'], $results['contrasena'])) {
+			$_SESSION['user_id'] = $results['id'];
+			header("location: ../index.php");
+		} else {
+			$message = "Sorry, those credentials do not match";
+		}
 	}
 ?>
-<!-- Hola mundo -->
-
 <!DOCTYPE html>
 <html lang="es">
 	<head>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Login || Register</title>
+		<title>Inicio de sesión</title>
 		<link rel="stylesheet" href="../css/styles.css" />
         <link rel="stylesheet" href="../css/Estilos.css">
 		<link rel="preconnect" href="https://fonts.googleapis.com" />
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 		<link href="https://fonts.googleapis.com/css2?family=Potta+One&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
 	</head>
-	
 	<body>
 		<!-- Inicio navbar -->
 		<nav class="navbar">
-			<a href="../index.php" class="logo">
+			<a href="../index.php" class="logo" >
 				<img src="../imgs/Mockup - deportfit.svg" alt="logo SportFit" />
 			</a>
-			<ul class="center-nav">
-				<li><a href="../index.php">Inicio</a></li>
-				<li><a href="inventario.php">Inventario</a></li>
-				<li><a href="prestamo.php">Préstamos</a></li>
-			</ul>
-			<a href="#" class="btn" id="iniciar-sesion">Iniciar sesión</a>
+			<span></span>
+			<span></span>
+			<span></span>
 		</nav>
 		<!-- Fin del navbar -->
 
 		<div class="contenedor__padre">
 			<div class="contenedor__todo">
 				<div class="caja__trasera">
-					<div class="caja__trasera-login">
-						<h3>¿Ya tienes una cuenta?</h3>
-						<p>Inicia sesión para entrar en la página.</p>
-						<button id="btn__iniciar-sesion">Iniciar Sesión</button>
-					</div>
 					<div class="caja__trasera-register">
 						<h3>¿Aún no tienes una cuenta?</h3>
 						<p>Regístrate para que puedas iniciar sesión.</p>
-						<button id="btn__registrarse">Registrarse</button>
+						<a href="signup.php">Registrarse</a>
 					</div>
 				</div>
 
 				<!-- Fomulario Login y Registro-->
 				<div class="contenedor__login-register">
 					<!-- Login -->
-					<form action="" class="formulario__login">
+					<form method="POST" class="formulario__login">
 						<h2>Iniciar Sesión</h2>
-						<input type="text" name="email" placeholder="Correo Electrónico" />
-						<input type="password" name="password" placeholder="Contraseña" />
+						<input type="text" name="correo" placeholder="Correo Electrónico" />
+						<input type="password" name="contrasena" placeholder="Contraseña" />
 						<button>Entrar</button>
-					</form>
-					<!-- Regístro -->
-					<form action="" class="formulario__register">
-						<h2>Registrarse</h2>
-						<input type="text" name="name" placeholder="Nombre Completo" />
-						<input type="text" name="email" placeholder="Correo Electrónico" />
-						<input type="text" name="username" placeholder="Usuario" />
-						<input type="password" name="password" placeholder="Contraseña" />
-						<button>Registrarse</button>
 					</form>
 				</div>
 			</div>
 		</div>
+		<?php if(!empty($message)): ?>
+			<div class="registered-user">
+				<p><?= $message ?></p>
+			</div>
+		<?php endif; ?>
 
-		<script src="../js/app.js"></script>
 	</body>
 </html>
